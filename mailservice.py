622863ef83c0150048edda794caa3068d2e9c97b
MehalaMohanan <70191218+MehalaMohanan@users.noreply.github.com>
2022-06-15 11:14:48 +0530

    just checking

--- mailservice.py
+++ mailservice.py
@@ -5,3 +5,4 @@ smtplibObj.starttls()
 smtplibObj.login("mysmart99mail@gmail.com","cjtbiwpobyyqvcpi")
 smtplibObj.sendmail("mysmart99mail@gmail.com","mysmart99mail@gmail.com","Subject:Home Footage\nHello there!!An unknown visitor is at your door!!")
 smtplibObj.quit()
+#nfhjnhdfkj
