72d2af293cb1a2242bd457dd86f9aac05df99761
HEMALSEBASTIAN <hemalsebastian123@gmail.com>
2022-06-21 12:15:45 +0530

    verification changed

--- faceverify.py
+++ faceverify.py
@@ -2,8 +2,9 @@ from deepface import DeepFace
 models = ["VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace", "DeepID", "ArcFace", "Dlib", "SFace"]
 detectors = ["opencv", "ssd", "mtcnn", "dlib", "retinaface"]
 metrics = ["cosine", "euclidean", "euclidean_l2"]
-verification=DeepFace.verify(img1_path="faces\\MEH.JPG",
-img2_path="faces\\m1.JPG",
-model_name = models[1], distance_metric = metrics[1],
+verification=DeepFace.verify(
+img1_path="faces\\ddd.jpg",
+img2_path="faces\\MEH.jpg",
+model_name = models[1], distance_metric = metrics[2],
 detector_backend = detectors[0])
 print(verification)
