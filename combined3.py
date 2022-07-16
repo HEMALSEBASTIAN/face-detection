de3c0bd28c18e73192515731adc6e364ee3ebed5
HEMALSEBASTIAN <hemalsebastian123@gmail.com>
2022-07-16 14:00:13 +0530

    Credentials added

--- combined3.py
+++ combined3.py
@@ -204,12 +204,10 @@ def email_send():
 
 def sms_send():
     global LINK,flag
-    account_sid = 'AC113e8a5153f1be30f820fe39994e56e1' 
-    auth_token = '0673d0b62b5fd5f2c1870cccb822b4e7' 
     client = Client(account_sid, auth_token) 
     
     message = client.messages.create(  
-                                messaging_service_sid='MGe7936fcc99775f919ae5dd2282a5bfb7', 
+                                messaging_service_sid=msg_sid, 
                                 body='ALERT; Video call link :'+str(LINK),      
                                 to='+918281565993 ' 
                             ) 
--- credentials.py
+++ credentials.py
@@ -1,2 +1,5 @@
 user_name='u1903092@rajagiri.edu.in'
 password='iamaHEROMAN@27!'
+account_sid = 'AC113e8a5153f1be30f820fe39994e56e1' 
+auth_token = '0673d0b62b5fd5f2c1870cccb822b4e7'
+msg_sid='MGe7936fcc99775f919ae5dd2282a5bfb7'
